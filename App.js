8c09fd0b44e8b411d5eba7df1b62239efe13ce22
WaokE <dlgudwns1207@gmail.com>
2024-03-15 01:17:59 +0900

    🎨 Clear App.js

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// 프레임워크 API
+import { StyleSheet, View } from "react-native";
+
+// 컴포넌트
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <View style={styles.appContainer}></View>;
 }
 
 const styles = StyleSheet.create({
-    container: {
+    appContainer: {
+        paddingTop: 20,
         flex: 1,
-        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
     },
