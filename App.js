148570144d2b45d857e0fd545b9deb9b4bc2521f
WaokE <dlgudwns1207@gmail.com>
2024-03-21 16:32:07 +0900

    🎨 Adjust App.js
    상태 바 관련 로직 추가
    약간의 구조 변화

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 // 프레임워크 API
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 // 컴포넌트
 import JinKazamaMove from "./screens/JinKazamaMove";
+import SelectCharacterScreen from "./screens/SelectCharacterScreen";
 
 export default function App() {
     return (
-        <LinearGradient colors={["#214C5C", "#000000"]} style={styles.appContainer}>
-            <JinKazamaMove />
-        </LinearGradient>
+        <>
+            <StatusBar />
+            <LinearGradient colors={["#214C5C", "#000000"]} style={styles.appContainer}>
+                {/* <JinKazamaMove /> */}
+                <SelectCharacterScreen />
+            </LinearGradient>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     appContainer: {
         flex: 1,
+        flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
     },
