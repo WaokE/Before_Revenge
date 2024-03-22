44e73c72ab9057fd769e1a22b25966c7f30e95e8
WaokE <dlgudwns1207@gmail.com>
2024-03-22 17:21:18 +0900

    ✨ Add remain character data

--- App.js
+++ App.js
@@ -22,10 +22,154 @@ export default function App() {
                         options={{ headerShown: false }}
                     />
                     <Stack.Screen
+                        name="AlisaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "alisa" }}
+                    />
+                    <Stack.Screen
+                        name="AsukaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "asuka" }}
+                    />
+                    <Stack.Screen
+                        name="AzucenaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "azucena" }}
+                    />
+                    <Stack.Screen
+                        name="BryanFuryMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "bryan-fury" }}
+                    />
+                    <Stack.Screen
+                        name="ClaudioSerafinoMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "claudio-serafino" }}
+                    />
+                    <Stack.Screen
+                        name="DevilJinMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "devil-jin" }}
+                    />
+                    <Stack.Screen
+                        name="FengWeiMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "feng-wei" }}
+                    />
+                    <Stack.Screen
+                        name="HwoarangMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "hwoarang" }}
+                    />
+                    <Stack.Screen
+                        name="Jack8MoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "jack-8" }}
+                    />
+                    <Stack.Screen
                         name="JinKazamaMoveScreen"
                         component={CharacterMoveScreen}
                         options={{ headerShown: false }}
                         initialParams={{ characterName: "jin-kazama" }}
+                    />
+                    <Stack.Screen
+                        name="JunKazamaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "jun-kazama" }}
+                    />
+                    <Stack.Screen
+                        name="KazuyaMishimaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "kazuya-mishima" }}
+                    />
+                    <Stack.Screen
+                        name="KingMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "king" }}
+                    />
+                    <Stack.Screen
+                        name="KumaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "kuma" }}
+                    />
+                    <Stack.Screen
+                        name="LarsAlexanderssonMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "lars-alexandersson" }}
+                    />
+                    <Stack.Screen
+                        name="LeeMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "lee" }}
+                    />
+                    <Stack.Screen
+                        name="LeoMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "leo" }}
+                    />
+                    <Stack.Screen
+                        name="LeroySmithMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "leroy-smith" }}
+                    />
+                    <Stack.Screen
+                        name="LiliMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "lili" }}
+                    />
+                    <Stack.Screen
+                        name="LingXiaoyuMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "ling-xiaoyu" }}
+                    />
+                    <Stack.Screen
+                        name="MarshallLawMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "marshall-law" }}
+                    />
+                    <Stack.Screen
+                        name="NinaWilliamsMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "nina-williams" }}
+                    />
+                    <Stack.Screen
+                        name="PandaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "panda" }}
+                    />
+                    <Stack.Screen
+                        name="PaulPhoenixMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "paul-phoenix" }}
+                    />
+                    <Stack.Screen
+                        name="RavenMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "raven" }}
                     />
                     <Stack.Screen
                         name="ReinaMoveScreen"
@@ -34,10 +178,40 @@ export default function App() {
                         initialParams={{ characterName: "reina" }}
                     />
                     <Stack.Screen
-                        name="DragunovMoveScreen"
+                        name="SergeiDragunovMoveScreen"
                         component={CharacterMoveScreen}
                         options={{ headerShown: false }}
                         initialParams={{ characterName: "sergei-dragunov" }}
+                    />
+                    <Stack.Screen
+                        name="ShaheenMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "shaheen" }}
+                    />
+                    <Stack.Screen
+                        name="SteveFoxMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "steve-fox" }}
+                    />
+                    <Stack.Screen
+                        name="VictorChevalierMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "victor-chevalier" }}
+                    />
+                    <Stack.Screen
+                        name="YoshimitsuMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "yoshimitsu" }}
+                    />
+                    <Stack.Screen
+                        name="ZafinaMoveScreen"
+                        component={CharacterMoveScreen}
+                        options={{ headerShown: false }}
+                        initialParams={{ characterName: "zafina" }}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
