4e3fea23073426931aae57a000dc826f1d0a6a24
WaokE <dlgudwns1207@gmail.com>
2024-03-31 18:19:53 +0900

    ✨ Add frame filter logic in filterMoveList
    - 필터링 함수 가독성 개선
    - 추가된 데이터 필드(넘패드 관련)에 따른 필터링 기능 추가(간단 테스트만 진행)

--- screens/CharacterMoveScreen.js
+++ screens/CharacterMoveScreen.js
@@ -124,27 +124,144 @@ const filterMoveList = (data, filterInput) => {
     const result = [];
     data.forEach((section) => {
         const filteredData = section.data.filter((move) => {
-            if (Object.values(filterInput.feature).every((value) => value === false)) {
-                return (
-                    move.command.join("").includes(filterInput.command.join("")) &&
-                    (move.name.includes(filterInput.text) || move.notes.includes(filterInput.text))
-                );
+            const commandMatches = move.command.join("").includes(filterInput.command.join(""));
+            const textMatches =
+                move.name.includes(filterInput.text) || move.notes.includes(filterInput.text);
+            let frameMatches = true;
+
+            // 유효한 프레임 필터가 있는 경우
+            if (
+                filterInput.frame.number !== "" &&
+                filterInput.frame.lossOrGain !== "UNSELECTED" &&
+                !isNaN(filterInput.frame.number)
+            ) {
+                const frameNumber = parseInt(filterInput.frame.number);
+
+                // 손해/이득 + 숫자만 들어오는 경우 -> 가드/히트 시 관계없이 손해/이득이 숫자랑 일치하는 경우만 리턴
+                if (
+                    filterInput.frame.hitOrGuard === "UNSELECTED" &&
+                    filterInput.frame.aboveOrBelow === "UNSELECTED"
+                ) {
+                    console.log("test");
+                    // 손해 프레임 검색
+                    if (filterInput.frame.lossOrGain === "손해가") {
+                        frameMatches =
+                            -parseInt(move.hitFrame) === frameNumber ||
+                            -parseInt(move.blockFrame) === frameNumber;
+                    }
+                    // 이득 프레임 검색
+                    if (filterInput.frame.lossOrGain === "이득이") {
+                        frameMatches =
+                            parseInt(move.hitFrame) === frameNumber ||
+                            parseInt(move.blockFrame) === frameNumber;
+                    }
+                }
+                // 손해/이득 + 숫자 + 이상/이하가 들어오는 경우 -> 가드/히트 시 관계없이 손해/이득이 숫자보다 크거나 작은 경우만 리턴
+                else if (filterInput.frame.hitOrGuard === "UNSELECTED") {
+                    if (filterInput.frame.aboveOrBelow === "이상") {
+                        if (filterInput.frame.lossOrGain === "손해가") {
+                            frameMatches =
+                                -parseInt(move.hitFrame) >= frameNumber ||
+                                -parseInt(move.blockFrame) >= frameNumber;
+                        }
+                        if (filterInput.frame.lossOrGain === "이득이") {
+                            frameMatches =
+                                parseInt(move.hitFrame) >= frameNumber ||
+                                parseInt(move.blockFrame) >= frameNumber;
+                        }
+                    }
+                    if (filterInput.frame.aboveOrBelow === "이하") {
+                        if (filterInput.frame.lossOrGain === "손해가") {
+                            frameMatches =
+                                -parseInt(move.hitFrame) <= frameNumber ||
+                                -parseInt(move.blockFrame) <= frameNumber;
+                        }
+                        if (filterInput.frame.lossOrGain === "이득이") {
+                            frameMatches =
+                                parseInt(move.hitFrame) <= frameNumber ||
+                                parseInt(move.blockFrame) <= frameNumber;
+                        }
+                    }
+                }
+                // 맞히고/막히고 + 손해/이득 + 숫자가 입력된 경우
+                else if (filterInput.frame.aboveOrBelow === "UNSELECTED") {
+                    if (filterInput.frame.hitOrGuard === "맞히고") {
+                        if (filterInput.frame.lossOrGain === "손해가") {
+                            frameMatches = -parseInt(move.hitFrame) === frameNumber;
+                        }
+                        if (filterInput.frame.lossOrGain === "이득이") {
+                            frameMatches = parseInt(move.hitFrame) === frameNumber;
+                        }
+                    }
+                    if (filterInput.frame.hitOrGuard === "막히고") {
+                        if (filterInput.frame.lossOrGain === "손해가") {
+                            frameMatches = -parseInt(move.blockFrame) === frameNumber;
+                        }
+                        if (filterInput.frame.lossOrGain === "이득이") {
+                            frameMatches = parseInt(move.blockFrame) === frameNumber;
+                        }
+                    }
+                }
+                // 맞히고/막히고 + 손해/이득 + 숫자 + 이상/이하가 입력된 경우
+                else {
+                    if (filterInput.frame.hitOrGuard === "맞히고") {
+                        if (filterInput.frame.aboveOrBelow === "이상") {
+                            if (filterInput.frame.lossOrGain === "손해가") {
+                                frameMatches = -parseInt(move.hitFrame) >= frameNumber;
+                            }
+                            if (filterInput.frame.lossOrGain === "이득이") {
+                                frameMatches = parseInt(move.hitFrame) >= frameNumber;
+                            }
+                        }
+                        if (filterInput.frame.aboveOrBelow === "이하") {
+                            if (filterInput.frame.lossOrGain === "손해가") {
+                                frameMatches = -parseInt(move.hitFrame) <= frameNumber;
+                            }
+                            if (filterInput.frame.lossOrGain === "이득이") {
+                                frameMatches = parseInt(move.hitFrame) <= frameNumber;
+                            }
+                        }
+                    }
+                    if (filterInput.frame.hitOrGuard === "막히고") {
+                        if (filterInput.frame.aboveOrBelow === "이상") {
+                            if (filterInput.frame.lossOrGain === "손해가") {
+                                frameMatches = -parseInt(move.blockFrame) >= frameNumber;
+                            }
+                            if (filterInput.frame.lossOrGain === "이득이") {
+                                frameMatches = parseInt(move.blockFrame) >= frameNumber;
+                            }
+                        }
+                        if (filterInput.frame.aboveOrBelow === "이하") {
+                            if (filterInput.frame.lossOrGain === "손해가") {
+                                frameMatches = -parseInt(move.blockFrame) <= frameNumber;
+                            }
+                            if (filterInput.frame.lossOrGain === "이득이") {
+                                frameMatches = parseInt(move.blockFrame) <= frameNumber;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!Object.values(filterInput.feature).some(Boolean)) {
+                return commandMatches && textMatches && frameMatches;
             } else {
                 return (
-                    move.command.join("").includes(filterInput.command.join("")) &&
-                    Object.keys(filterInput.feature).every(
-                        (key) =>
-                            (filterInput.feature[key] === true && move.feature.includes(key)) ||
-                            filterInput.feature[key] === false
+                    commandMatches &&
+                    Object.entries(filterInput.feature).every(([key, value]) =>
+                        value ? move.feature.includes(key) : true
                     ) &&
-                    (move.name.includes(filterInput.text) || move.notes.includes(filterInput.text))
+                    textMatches &&
+                    frameMatches
                 );
             }
         });
+
         if (filteredData.length !== 0) {
             result.push({ title: section.title, data: filteredData });
         }
     });
+
     return result;
 };
 
