614e8b0b120c8eecf52609a31ee88b8a53399164
WaokE <dlgudwns1207@gmail.com>
2024-04-04 17:26:20 +0900

    🔧 Adjust folder architecture
    의미없던 폴더를 삭제하고 병합하여 의미하기 쉬운 구조를 갖도록 개선

--- components/MoveContainer.js
+++ components/MoveContainer.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 
-import TextWithHighlight from "./UI/TextWithHightlight";
+import TextWithHighlight from "./TextWithHightlight";
 
 const sampleMove = {
     name: "Sample Move",
