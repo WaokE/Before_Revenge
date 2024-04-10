c5a3456575e75df49f7338945394ad845c5b20a8
WaokE <dlgudwns1207@gmail.com>
2024-04-10 21:27:13 +0900

    ✨ Add Pretendard Fonts

--- App.js
+++ App.js
@@ -15,7 +15,15 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     const [fontsLoaded, fontError] = useFonts({
-        tekkenfont: require("./assets/fonts/tekkenfont.ttf"),
+        "Pretendard-Black": require("./assets/fonts/Pretendard-Black.otf"),
+        "Pretendard-Bold": require("./assets/fonts/Pretendard-Bold.otf"),
+        "Pretendard-ExtraBold": require("./assets/fonts/Pretendard-ExtraBold.otf"),
+        "Pretendard-ExtraLight": require("./assets/fonts/Pretendard-ExtraLight.otf"),
+        "Pretendard-Light": require("./assets/fonts/Pretendard-Light.otf"),
+        "Pretendard-Medium": require("./assets/fonts/Pretendard-Medium.otf"),
+        "Pretendard-Regular": require("./assets/fonts/Pretendard-Regular.otf"),
+        "Pretendard-SemiBold": require("./assets/fonts/Pretendard-SemiBold.otf"),
+        "Pretendard-Thin": require("./assets/fonts/Pretendard-Thin.otf"),
     });
 
     const onLayoutRootView = useCallback(async () => {
