dd906559df11450632ca8f43576cf5fabd9b781b
WaokE <dlgudwns1207@gmail.com>
2024-04-11 04:33:51 +0900

    ✨ Top side UI Layout of new CharacterMoveScreen
    - 현재 보고 있는 캐릭터 이름 표기
    - 검색 창(기능 X)
    - 섹션 칩(기능 O)

--- screens/CharacterMoveScreen.js
+++ screens/CharacterMoveScreen.js
@@ -9,9 +9,9 @@ import {
     Text,
     TextInput,
     SafeAreaView,
+    Dimensions,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { useState, useEffect, memo } from "react";
+import { useState, useEffect } from "react";
 import { Icon } from "@rneui/themed";
 
 // 컴포넌트
@@ -25,262 +25,140 @@ import convertFeature from "../lib/convertDataToImage/convertFeature";
 import importCharacterMoveData from "../lib/importCharacterMoveData";
 import filterMoveList from "../lib/filterMoveList";
 import initialFilterInput from "../lib/initialFilterInput";
+import { convertCharNameEngToKor } from "../lib/convertCharNameBetweenEngKor";
 
-const CharacterMoveScreen = ({ route }) => {
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const CharacterMoveScreen = ({ route, navigation }) => {
     const { characterName } = route.params;
     const moveData = importCharacterMoveData[characterName];
-    const [filterInput, setFilterInput] = useState(initialFilterInput);
-    const [tempFilterInput, setTempFilterInput] = useState(initialFilterInput);
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        console.log("CharacterMoveScreen rendered, clear filter input");
-        setFilterInput(initialFilterInput);
-        setTempFilterInput(initialFilterInput);
-    }, []);
+    const [selectedSection, setSelectedSection] = useState("전체");
+
+    useEffect(() => {}, []);
 
     return (
-        <SafeAreaView style={styles.moveScreen}>
-            <View style={{ backgroundColor: "#363636", flex: 1 }}>
-                <MoveListFilterKeyboard
-                    filterInput={filterInput}
-                    onChangeFilterInput={setFilterInput}
-                    isModalOpen={isModalOpen}
-                    setIsModalOpen={setIsModalOpen}
-                    tempFilterInput={tempFilterInput}
-                    setTempFilterInput={setTempFilterInput}
-                />
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        backgroundColor: "white",
-                        borderRadius: 5,
-                    }}
-                >
-                    <TextInput
-                        onChangeText={(text) => setFilterInput((prev) => ({ ...prev, text }))}
-                        value={filterInput.text}
-                        style={{ flex: 1, textAlign: "center" }}
-                        placeholder="기술명, 설명 검색 (ex.초풍신권, 컷킥...)"
-                    />
-                    <Button
-                        title="필터 추가"
-                        onPress={() => setIsModalOpen((prev) => !prev)}
-                        color="#006666"
-                    />
+        <View style={styles.moveScreen}>
+            <View style={styles.headerContainer}>
+                <Pressable onPress={() => navigation.goBack()}>
+                    <Icon name="arrow-back" color="white" />
+                </Pressable>
+                <Text style={styles.headerText}>{convertCharNameEngToKor(characterName)}</Text>
+                <View style={{ marginLeft: "auto" }}>
+                    <Icon name="close" color="white" />
                 </View>
-                <View style={{ flexDirection: "row", gap: 4 }}>
-                    {Object.keys(filterInput.feature).some(
-                        (key) => filterInput.feature[key] === true
-                    ) && (
-                        <Pressable
-                            onPress={() => {
-                                setFilterInput((prev) => ({
-                                    ...prev,
-                                    feature: { HM: false, HT: false, PC: false, TN: false },
-                                }));
-                                setTempFilterInput((prev) => ({
-                                    ...prev,
-                                    feature: { HM: false, HT: false, PC: false, TN: false },
-                                }));
-                            }}
-                        >
-                            <View style={styles.showCurrentFilterItem}>
-                                {filterInput.feature.HM && (
-                                    <Image
-                                        source={require("../assets/FeatureIcon/HMicon.png")}
-                                        style={{ width: 20, height: 20 }}
-                                    />
-                                )}
-                                {filterInput.feature.HT && (
-                                    <Image
-                                        source={require("../assets/FeatureIcon/HTicon.png")}
-                                        style={{ width: 20, height: 20 }}
-                                    />
-                                )}
-                                {filterInput.feature.PC && (
-                                    <Image
-                                        source={require("../assets/FeatureIcon/PCicon.png")}
-                                        style={{ width: 20, height: 20 }}
-                                    />
-                                )}
-                                {filterInput.feature.TN && (
-                                    <Image
-                                        source={require("../assets/FeatureIcon/TNicon.png")}
-                                        style={{ width: 20, height: 20 }}
-                                    />
-                                )}
-                                <Icon name="close" size={20} color={"white"} />
-                            </View>
-                        </Pressable>
-                    )}
-                    {filterInput.hitLevel.length > 0 && (
-                        <Pressable
-                            onPress={() => {
-                                setTempFilterInput((prev) => {
-                                    return {
-                                        ...prev,
-                                        hitLevel: [],
-                                    };
-                                });
-                                setFilterInput((prev) => {
-                                    return {
-                                        ...prev,
-                                        hitLevel: [],
-                                    };
-                                });
-                            }}
-                        >
-                            <View style={styles.showCurrentFilterItem}>
-                                <Text style={{ color: "white" }}>
-                                    {filterInput.hitLevel.join(" ")}
-                                </Text>
-                                <Icon name="close" size={20} color={"white"} />
-                            </View>
-                        </Pressable>
-                    )}
-                    {(filterInput.frame.hitOrGuard !== "UNSELECTED" ||
-                        filterInput.frame.lossOrGain !== "UNSELECTED" ||
-                        filterInput.frame.number !== "" ||
-                        filterInput.frame.aboveOrBelow !== "UNSELECTED") && (
-                        <Pressable
-                            onPress={() => {
-                                setFilterInput((prev) => ({
-                                    ...prev,
-                                    frame: {
-                                        number: "",
-                                        lossOrGain: "UNSELECTED",
-                                        hitOrGuard: "UNSELECTED",
-                                        aboveOrBelow: "UNSELECTED",
-                                    },
-                                }));
-                                setTempFilterInput((prev) => ({
-                                    ...prev,
-                                    frame: {
-                                        number: "",
-                                        lossOrGain: "UNSELECTED",
-                                        hitOrGuard: "UNSELECTED",
-                                        aboveOrBelow: "UNSELECTED",
-                                    },
-                                }));
-                            }}
-                        >
-                            <View style={styles.showCurrentFilterCommand}>
-                                {filterInput.frame.hitOrGuard !== "UNSELECTED" && (
-                                    <Text style={{ color: "white" }}>
-                                        {filterInput.frame.hitOrGuard}
-                                    </Text>
-                                )}
-                                {filterInput.frame.lossOrGain !== "UNSELECTED" && (
-                                    <Text style={{ color: "white" }}>
-                                        {filterInput.frame.lossOrGain}
-                                    </Text>
-                                )}
-                                {filterInput.frame.number !== "" && (
-                                    <Text style={{ color: "white" }}>
-                                        {filterInput.frame.number}
-                                    </Text>
-                                )}
-                                {filterInput.frame.aboveOrBelow !== "UNSELECTED" && (
-                                    <Text style={{ color: "white" }}>
-                                        {filterInput.frame.aboveOrBelow}
-                                    </Text>
-                                )}
-                                <Icon name="close" size={20} color={"white"} />
-                            </View>
-                        </Pressable>
-                    )}
-                    {filterInput.command.length > 0 && (
-                        <Pressable
-                            onPress={() => {
-                                setFilterInput((prev) => ({ ...prev, command: [] }));
-                                setTempFilterInput((prev) => ({ ...prev, command: [] }));
-                            }}
-                        >
-                            <View style={styles.showCurrentFilterItem}>
-                                {filterInput.command.map((item, index) => (
-                                    <View key={index}>{convertCommand([item])}</View>
-                                ))}
-                                <Icon name="close" size={20} color={"white"} />
-                            </View>
-                        </Pressable>
-                    )}
-                </View>
-                <MemoizedMoveList data={moveData} filterInput={filterInput} />
             </View>
-        </SafeAreaView>
+            <View style={styles.searchInputContainer}>
+                <Icon name="search" color="#6B6B6B" size={20} />
+                <TextInput
+                    placeholder="기술명 검색(초풍...)"
+                    placeholderTextColor={"#6B6B6B"}
+                    style={styles.searchInput}
+                    onChangeText={(text) => null}
+                />
+            </View>
+            <View style={styles.filterChipContainer}>
+                <Pressable
+                    style={
+                        selectedSection === "전체" ? styles.filterChipSelected : styles.filterChip
+                    }
+                    onPress={() => setSelectedSection("전체")}
+                >
+                    <Text
+                        style={
+                            selectedSection === "전체"
+                                ? styles.filterChipTextSelected
+                                : styles.filterChipText
+                        }
+                    >
+                        전체
+                    </Text>
+                </Pressable>
+                {moveData.map((section) => (
+                    <Pressable
+                        style={
+                            selectedSection === section.title
+                                ? styles.filterChipSelected
+                                : styles.filterChip
+                        }
+                        key={section.title}
+                        onPress={() => setSelectedSection(section.title)}
+                    >
+                        <Text
+                            style={
+                                selectedSection === section.title
+                                    ? styles.filterChipTextSelected
+                                    : styles.filterChipText
+                            }
+                        >
+                            {section.title}
+                        </Text>
+                    </Pressable>
+                ))}
+            </View>
+            <View style={styles.moveListContainer}></View>
+        </View>
     );
 };
 
-const MemoizedMoveList = memo(({ data, filterInput }) => {
-    console.log("MoveList rendered");
-
-    const filteredMoveList = filterMoveList(data, filterInput);
-
-    return filteredMoveList.length === 0 ? (
-        <NoFilterResult />
-    ) : (
-        <SectionList
-            sections={filteredMoveList}
-            renderItem={({ item }) => (
-                <MoveContainer
-                    name={item.name ? item.name : "이름 없음"}
-                    command={convertCommand(item.command)}
-                    hitLevel={item.hitLevel}
-                    damage={item.damage}
-                    startUpFrame={item.startUpFrame}
-                    blockFrame={item.blockFrame}
-                    hitFrame={item.hitFrame}
-                    counterHitFrame={item.counterHitFrame}
-                    feature={convertFeature(item.feature)}
-                    notes={item.notes}
-                    highlight={filterInput.text}
-                />
-            )}
-            renderSectionHeader={({ section: { title } }) => (
-                <Text style={styles.sectionHeaderText}>{title}</Text>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-            extraData={filterInput}
-            stickySectionHeadersEnabled={true}
-        />
-    );
-});
-
 const styles = StyleSheet.create({
     moveScreen: {
-        borderRadius: 10,
         flex: 1,
+        backgroundColor: "#3E3E3E",
     },
-    sectionHeaderText: {
+    headerContainer: {
+        height: windowHeight * 0.08,
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: 15,
+    },
+    headerText: {
         color: "white",
         fontSize: 20,
-        fontWeight: "bold",
-        textAlign: "center",
+        fontFamily: "Pretendard-Bold",
+        marginLeft: 10,
+    },
+    searchInputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
         backgroundColor: "#202124",
-        borderBottomWidth: 1,
-        borderBottomColor: "white",
-        marginHorizontal: 4,
         padding: 8,
+        marginHorizontal: 10,
+        gap: 10,
+        borderRadius: 5,
     },
-    showCurrentFilterItem: {
-        flexDirection: "row",
-        borderWidth: 1,
-        borderColor: "white",
-        borderRadius: 4,
-        padding: 4,
-        margin: 4,
+    searchInput: {
+        color: "white",
+        flex: 1,
     },
-    showCurrentFilterCommand: {
+    filterChipContainer: {
         flexDirection: "row",
-        borderWidth: 1,
-        borderColor: "white",
-        borderRadius: 4,
-        padding: 4,
-        margin: 4,
-        gap: 2,
+        gap: 10,
+        marginVertical: 10,
+        marginHorizontal: 10,
+    },
+    filterChip: {
+        backgroundColor: "transparent",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 18,
+    },
+    filterChipSelected: {
+        backgroundColor: "#202124",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 18,
+    },
+    filterChipText: {
+        color: "#6B6B6B",
+    },
+    filterChipTextSelected: {
+        color: "white",
+    },
+    moveListContainer: {
+        flex: 1,
+        backgroundColor: "#363636",
     },
 });
 
